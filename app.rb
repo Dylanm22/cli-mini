d54edaca79537ef013ef4dd250810b5bdf280572
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-01-11 17:59:08 +0000

    cli2

---
+++ app.rb
@@ -0,0 +1,5 @@
+require_relative "movie"
+
+movie = Movie.new
+
+movie.this
--- movie.rb
+++ movie.rb
@@ -2,17 +2,63 @@
 class Movie
   attr_accessor :genre
 
-  def initialize (genre)
-   
+  def initialize
     @genre = genre
   
   end
 
-  def genre=(value)
-    unless value.is_a?(String)
- raise TypeError, "genre must be string"
-	end
-	end
+  #def genre=(value)
+    #unless value.is_a?(String)
+ #raise TypeError, "genre must be string"
+	#end
+	#end
+ # movie_genre = Movie.new("horror")
+ # movie_genre = movie_genre.genre
+  #pp movie_genre
+
+def this
+puts"what genre of movie would you want to watch today"
+  puts "1. Comedy "
+  puts "2. action "
+  puts "3. horror"
+  puts "4. si-fi "
+  puts "5. family "
+  puts "\n"
+  puts "Enter a number"
+  self.genre =gets.chomp
+
+  
+ choice = @genre.to_i
+  case choice
+  when 1
+      comedy
+  when 2 
+    action
+    end
 
 end
 
+def comedy
+  puts "\n"
+  puts "The comedy movies in theaters today are:"
+  puts"\n"
+  puts "Wonka"
+end
+
+def action
+  puts "\n"
+  puts "The action movies in theaters today are:"
+  puts"\n"
+  puts "The Beekeeper"
+  puts"\n"
+  puts"Aquaman and the Lost Kingdom"
+  puts"\n"
+  puts"The Iron Claw"
+
+end
+
+
+
+
+  
+end
