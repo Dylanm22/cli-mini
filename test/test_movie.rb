6918e7740a01305cf6e36fe603c5ebffa4a4791e
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-02-01 18:18:30 +0000

    movie

--- test/test_movie.rb
+++ test/test_movie.rb
@@ -1,9 +1,19 @@
-require 'minitest/autorun'
+ require 'minitest/autorun'
 require './movie'
+
+# class TestMovie < Minitest::Test
+#   def test_genre
+#      Movie(:genre) 
+#     assert_equal comedy,  "method failed"
+#   end
+# end
+
+ 
 
 class TestMovie < Minitest::Test
   def test_genre
-     movie = Movie.new
-    assert_equal  1, movie.this, "Addition method failed"
+    movie = Movie.new
+    movie.genre = 'comedy'
+    assert_equal 'comedy', movie.genre, "Genre method failed"
   end
 end
